2100db81b3a73c5e427127ee6a80ecbc4bf94263
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2023-09-08 21:16:16 +0300

    add file

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,21 @@
+// import css from './Statistics.module.css';
+// // import from './Feedback'
+
+// const Statistics = () => {
+//   return (
+//     <div>
+//       <h2 className={css.title}>Statistics</h2>
+//       <div className={css.review}>
+//         <p>Good: 0</p>
+//         <p>Neutral: 0</p>
+//         <p>Bad: 0</p>
+//       </div>
+//       <div>
+//         <p>Total: 0</p>
+//         <p>Pozitive feedback: 0</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Statistics;
